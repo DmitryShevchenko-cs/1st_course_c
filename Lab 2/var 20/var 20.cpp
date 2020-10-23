566a8421c689b1afcdae64db105d8cc287130412
Dmitry <dima.shevchenko10@gmail.com>
2020-10-24 00:46:07 +0300

    коректировки

--- Lab 2/var 20/var 20.cpp
+++ Lab 2/var 20/var 20.cpp
@@ -1,6 +1,22 @@
-﻿#include <stdio.h>
-#include <iostream>
+﻿#include <iostream>
+#include <stdio.h>
 
-int main(void) {
+int main(void){
+	setlocale(LC_ALL,"ru");
+
+	char name1[9], name2[9], name3[9];
+	char type1[1], type2[1], type3[1];
+	short int tem1, tem2, tem3;
+	unsigned int sp1, sp2, sp3;
+
+	printf("1. Введите: название, школу, количество, площадь >");
+	scanf_s("%s %c %hd %u", name1, &type1, &tem1, &sp1);
+
+	printf("2. Введите: название, школу, количество, площадь >");
+	scanf_s("%s %c %hd %u", name2, &type2, &tem2, &sp2);
+
+	printf("3. Введите: название, школу, количество, площадь >");
+	scanf_s("%s %c %hd %u", name3, &type3, &tem3, &sp3);
+	
 
 }
--- Lab 3/var20/var20.cpp
+++ Lab 3/var20/var20.cpp
@@ -1,8 +1,24 @@
 ﻿#include <stdio.h>
 #include <iostream>
+#include <math.h>
 
-int main()
-{
-   
+double x, a, n, m;
+double lg;
+long double t1, t2;
+int main(void){
+
+	printf("input x:");
+	scanf_s("%lf", &x);//2
+	printf("input a:");
+	scanf_s("%lf", &a);//3
+	printf("input n:");
+	scanf_s("%lf", &a);//4
+	printf("input m:");
+	scanf_s("%lf", &a);//5
+	
+	lg = ((pow(double(a), 2) + pow(double(x), 2)) / (pow(double(a), 2) - pow(double(x), 2)));
+	t1 = (1/(4*pow(double(a), 3)))*log(lg);
+	t2 = ( pow(cos(a * x), n - 1)) / (a * (m - 1) * pow( sin(a * x), m - 1));
+	printf("t1= %lf\n", t1);
+	printf("t2= %lf", t2);
 }
-
