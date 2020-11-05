5efc53b55ae65ddb12a45f1b94d11c251dacd128
Dmitry <dima.shevchenko10@gmail.com>
2020-11-05 12:22:16 +0200

    изменены условия в условных операторах

--- Lab 3/var20/var20.cpp
+++ Lab 3/var20/var20.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void){
 
 	double x;
-	double a = 5.4;
+	double a = 7.5;
 	double n = 2, m = 2;
 	double ax, l;
 	double t1, t2;
@@ -13,13 +13,11 @@ int main(void){
 	printf("Input numbers: x> ");
 	scanf("%lf", &x);
 	l = (pow(a, 2) + pow(x, 2)) / ((a - x) * (a + x));//ОДЗ для логарифма
-	
-	if (l > 0) {
+	if (x < a) {
 		
 		t1 = (1 / (4 * pow(a, 3))) * log(l);
 		ax = a * x;
 		t2 = (pow(cos(ax * 3.14 / 180), n - 1)) / (a * (m - 1) * pow(sin(ax * 3.14 / 180), m - 1));
-
 		printf("t1= %lg\n", t1);  //lg-тоже самое что и lf, но исключает незначущие нули
 		printf("t2= %lg\n", t2);
 	}
