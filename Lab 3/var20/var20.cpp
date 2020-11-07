21a5b34835a6549e99ae18136ba75078eaf27aa6
Dmitry <dima.shevchenko10@gmail.com>
2020-11-07 18:23:47 +0200

    добавил условие х!=0 тогда в знаменатиле будет 0 - неопределенность

--- Lab 3/var20/var20.cpp
+++ Lab 3/var20/var20.cpp
@@ -7,18 +7,18 @@ int main(void){
 	double x;
 	double a = 7.5;
 	double n = 2, m = 2;
-	double ax, l;
+	double с, l;
 	double t1, t2;
 
 	printf("Input numbers: x> ");
 	scanf("%lf", &x);
-	l = (pow(a, 2) + pow(x, 2)) / ((a - x) * (a + x));//ОДЗ для логарифма
-	if (x < a) {
+	l = (pow(a, 2) + pow(x, 2)) / ((a - x) * (a + x));	//ОДЗ для логарифма
+	if (x < a && x!=0) {	//если х=0 то под знаком логарифма будет 1, а логарифм 1 это 0
 		
 		t1 = (1 / (4 * pow(a, 3))) * log(l);
-		ax = a * x;
-		t2 = (pow(cos(ax * 3.14 / 180), n - 1)) / (a * (m - 1) * pow(sin(ax * 3.14 / 180), m - 1));
-		printf("t1= %lg\n", t1);  //lg-тоже самое что и lf, но исключает незначущие нули
+		с = a*x * 3.14 / 180;
+		t2 = (pow(cos(с), n - 1)) / (a * (m - 1) * pow(sin(с), m - 1));
+		printf("t1= %lg\n", t1);	//lg-тоже самое что и lf, но исключает незначущие нули
 		printf("t2= %lg\n", t2);
 	}
 
