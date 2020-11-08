71e78490b66b45417d259d686db335b49015f113
Dmitry <dima.shevchenko10@gmail.com>
2020-11-08 18:02:36 +0200

    lab 5 was added

---
+++ Lab 5/var 20/var 20.cpp
@@ -0,0 +1,32 @@
+﻿#define  _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+//#include <conio.h> // для _getch
+#include "windows.h"
+
+int main(void) {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	double x, y;
+
+	while (true)
+	{
+		//system("cls"); //офистка экрана вывода
+
+		printf("Введите координату x >");
+		scanf("%lf", &x);
+		printf("Введите координату y >");
+		scanf("%lf", &y);
+
+		printf("x=%.3lf;  y=%.3lf\n", x, y);
+
+		if (((x <= 1) && (y >= -x - 1)) or ((x >= 1) && (y <= -x + 1)))     
+
+			printf("Точка попадает в область\n\n");
+		 
+		else printf("Точка не попадает в область\n\n");
+
+		//printf("Для продолжения нажмите любую клавишу");
+		//_getch(); //ждет нажатия кнопки
+	}
+	return 0; 
+}
