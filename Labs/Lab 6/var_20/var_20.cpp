26012be8a5a9d521f8cfbc1b492e28e4d6e22dc1
Dmitry <dima.shevchenko10@gmail.com>
2020-11-19 17:17:23 +0200

    Lab 6 finished

--- Labs/Lab 6/var_20/var_20.cpp
+++ Labs/Lab 6/var_20/var_20.cpp
@@ -1,16 +1,33 @@
-﻿#include <iostream>
-using namespace std;
-
+﻿#include <stdio.h>
+#include <math.h>
+#include "windows.h"
 
 int main(void) {
-	long n = 0;
-	double a = 0, S = 0, k1, k2;
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
 
-	k1 = pow(-1, n);
-	k2 = 2 * pow(n, 2) + 1;
+    long n; 
+    double dbln;
+    double sum = 0;        
+    double a;         
+    const long double eps = 0.000001;  
+    short k1 = 1;         
+      
 
-	for (int n = 0; n <= 9; n++) {
-		S = k1 * ((n - 1) / k2);
-		printf("%lf\n", S);
-	}
+    for (n = 0; ; n++, k1 *= -1) {
+        dbln = n;
+        a = k1 * (dbln + 1) / (2 * dbln * dbln + 1);
+
+        if (fabs(a) >= eps) {
+            sum += a;      
+        }
+        else break;
+
+        if (n == 9) {
+            printf("Сумма 10 членов ряда = %10.7lf\n", sum);
+        }
+    }
+
+    printf("Полная сумма ряда = %10.7lf\n", sum);
+    return 0;
 }
