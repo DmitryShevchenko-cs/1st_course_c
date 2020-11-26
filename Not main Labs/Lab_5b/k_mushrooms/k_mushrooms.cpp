567f3e33fbf398952568b4c4554b8b1241eda1e7
Dmitry <dima.shevchenko10@gmail.com>
2020-11-26 12:26:31 +0200

    теперь к- это только положительные числа

--- Not main Labs/Lab_5b/k_mushrooms/k_mushrooms.cpp
+++ Not main Labs/Lab_5b/k_mushrooms/k_mushrooms.cpp
@@ -1,30 +1,32 @@
 ﻿#define  _CRT_SECURE_NO_WARNINGS
 #include "windows.h"
 #include <iostream>
+using namespace std;
 
 int main(void) {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    long long k;
+    unsigned k;
 
     while (true) {
 
         printf("Сколько у вас грибов?");
-        scanf(" У меня %lld", &k);
+        scanf("%u", &k);
+        //cin >> k;
 
         if (k % 10 == 1 && k % 100 != 11) 
-            printf("У меня %lld гриб\n\n", k);
+            printf("У меня %u гриб\n\n", k);
 
         if ((k % 10 == 2 || k % 10 == 3 || k % 10 == 4) && 
             k % 100 != 12 && k % 100 != 13 && k % 100 != 14)
-            printf("У меня %lld гриба\n\n", k);
+            printf("У меня %u гриба\n\n", k);
 
         if (k % 10 > 4 || k % 10 == 0)
-            printf("У меня %lld грибов\n\n", k);
+            printf("У меня %u грибов\n\n", k);
 
         if (k % 100 == 11 || k % 100 == 12 || k % 100 == 13 || k % 100 == 14)
-            printf("У меня %lld грибов\n\n", k);
+            printf("У меня %u грибов\n\n", k);
 
     }
 
