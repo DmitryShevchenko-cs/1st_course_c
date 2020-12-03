c7bfdcfe0c134ecd30653402f32a335f383804a1
Dmitry <dima.shevchenko10@gmail.com>
2020-12-03 09:19:42 +0200

    Lab 6a finished

---
+++ Not main Labs/Lab_6a/computer`s num/computer`s num.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int num1, num2;
+	srand(time(NULL));
+
+	num1 = rand() % 100;
+	cout << num1 << endl;
+
+	for (int i = 0; ; ) {
+
+		cin >> num2;
+		i++;
+			if (num2 == num1) {
+				cout << "Ура вы отгадали число" << endl;
+				cout << "Количество попыток:" << i << endl;
+			}
+			else cout << "";
+}
+	return 0;
+}
