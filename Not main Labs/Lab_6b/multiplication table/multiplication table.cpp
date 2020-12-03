35baab068e035913782cb5cd77a658a15ddae1bc
Dmitry <dima.shevchenko10@gmail.com>
2020-12-03 09:39:53 +0200

    lab 6a finished

---
+++ Not main Labs/Lab_6b/multiplication table/multiplication table.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <ctime>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+	srand(time(NULL));
+
+	int result;
+	int mark = 0;
+
+	for (int i = 0; i < 10; i++) {
+		
+		int num1 = rand() % 10,
+			num2 = rand() % 10;
+	
+		cout << num1 << "*" << num2 << "=";
+		cin >> result;
+
+		if (result == num1 * num2) mark++;
+		else cout << "";
+	}
+
+	cout << "Your mark is:" << mark << "/10" << endl;
+	return 0;
+}
