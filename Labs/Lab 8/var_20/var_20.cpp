45ebef536fdb8e17056ccdca792ac3d5e6bafbd8
Dmitry <dima.shevchenko10@gmail.com>
2020-12-10 00:12:55 +0200

    лАба 8 добавлена, но не закончина

---
+++ Labs/Lab 8/var_20/var_20.cpp
@@ -0,0 +1,93 @@
+﻿#include <iostream>
+#include <ctime>
+#include <windows.h>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int const size = 50;
+	int size2 = 0;
+	int arr[size]{};
+	double Arr[size]{};
+	int i, j;
+	double av = 0;
+	int nn, nn_max=0;
+	int ib;
+
+
+	srand(time(NULL));
+
+	for (i = 0; i < size; i++) arr[i] = rand() % 100 - 50;
+
+	printf("Начальный массив:\n");
+
+	for (i = 0; i < size; printf("%3d  ", arr[i++]));
+	putchar('\n');
+	putchar('\n');
+	///////////////////////////////////////////////////////////////////////////
+	double max = -1;
+
+	for (nn = i = 0; i < size; i++) {
+
+		
+		
+
+		if (arr[i] < 0) {
+			if (!nn) {										//n=0?
+
+				ib = i;
+				av = arr[i];
+				nn = 1;
+				
+			}
+
+			else {
+
+				av += arr[i];
+				nn++;
+			
+			}
+		}
+
+		else {
+			if (nn) {										//nn!=0
+				av = fabs(av / nn);
+
+				if (nn > 1) 
+					if (av > max) {
+
+						max = av;
+						nn_max = nn;
+					}
+				
+				nn = 0;
+			}
+		}
+	}
+
+	if (nn) {										//nn!=0
+
+		av = fabs(av / nn);
+		
+		if (nn > 1) 
+			if (av > max) {
+				max = av;
+				nn_max = nn;	
+			}
+
+		nn = 0;
+	}
+
+	cout << "\n\nНаибольшее абсолютное значение среднего арифметического= " << max << "\n";
+	cout << "Массив-результат: "; 
+	
+	
+
+	
+	
+	return 0;
+}
---
+++ Not main Labs/Lab 8В/number/number.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    long int num_a;
+    int sum=0, k=0;
+    long int num_b;
+    int temp;
+
+    cin >> num_a;
+    while (num_a != 0) {
+        
+        temp = num_a % 10;
+        cout << temp << "" ;
+       
+        
+        sum += temp;
+
+        num_a /= 10;
+        k++;
+
+    }
+
+    cout << "\n\n" << sum << endl;
+    cout << k << endl;
+   
+
+    return 0;
+}
+
