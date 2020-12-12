4fd232716cedc1b0646707e0a88c654c3a804c0c
Dmitry <dima.shevchenko10@gmail.com>
2020-12-12 16:06:14 +0200

    9 лаба добавлена

---
+++ Labs/Lab 9/var_20/var_20.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <ctime>
+#include <windows.h>
+#define S 9
+using namespace std;
+
+short arr[S][S]{};
+short i, j, k;
+short r1, r2;
+short sum_i, sum_j, sum;
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	
+	srand(time(NULL));
+	
+	r1 = 0;
+
+	for (i = 0; i < S; i++) {            //строка
+
+		for (j = 0; j < S; j++) {        //столбец
+			arr[i][j] = rand() % 99;
+		}
+	}
+	cout << "Начальный массив :" << endl;
+	for (i = 0; i < S; i++) {            
+
+		for (j = 0; j < S; j++) {        
+			printf("%4d", arr[i][j]);
+		}
+		cout << endl;
+	}
+	cout << endl;
+	cout << endl;
+
+	for (k = 8; k >= 0; k--) {            
+		for (j = 0; j < S; j++) {
+			
+			//столбец
+			sum_j += arr[r1][j];
+			
+			//линия
+			sum_i += arr[j][k];
+
+			
+		}
+		
+		arr[r1][k] = sum_i + sum_j - arr[r1][k];
+		r1++;
+		sum_i = 0;
+		sum_j = 0;
+		
+	}
+
+	cout << "Массив результат :" << endl;
+
+	for (i = 0; i < S; i++) {            
+
+		for (j = 0; j < S; j++) {        
+			printf("%5d", arr[i][j]);
+		}
+		cout << endl;
+	}
+
+}
