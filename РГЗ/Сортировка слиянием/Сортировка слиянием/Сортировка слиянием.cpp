aed0980961245a3cf79ace3446c8b77d02efee82
Dmitry <dima.shevchenko10@gmail.com>
2020-12-17 00:22:21 +0200

    таблицы

--- РГЗ/Сортировка слиянием/Сортировка слиянием/Сортировка слиянием.cpp
+++ РГЗ/Сортировка слиянием/Сортировка слиянием/Сортировка слиянием.cpp
@@ -7,29 +7,16 @@
 
 using namespace std;
 
-int merge(int* arr, int N) {
-	int buff = 0; // для хранения перемещаемого значения
-	int i, j;
-
-	for (i = 1; i < N; i++)
-	{
-		buff = arr[i];
-		for (j = i - 1; j >= 0 && arr[j] > buff; j--)
-			arr[j + 1] = arr[j];
-
-		arr[j + 1] = buff;
-	}
-
-	return 0;
+void merge_sort(int* unsorted, unsigned int size)
+{
 }
-
 
 int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
 	int n;
-	int size = 5;
+	int size = 10;
 	int a = size, b = 1;
 	int* Arr = new int[size];
 	double dur_arr[m]{};
@@ -39,15 +26,15 @@ int main() {
 
 	for (int i = 0; i < m; i++)
 	{
-		for (n = 0; n < size; n++) { // upor
+		for (n = 0; n < size; n++) { // +
 			Arr[n] = b++;
 		}
-		size += 5;
+		//size += 5;
 
 		auto start = chrono::high_resolution_clock::now();
 
-		merge(Arr, size);
-
+		merge_sort(Arr, size);
+		cout << endl;
 		auto end = chrono::high_resolution_clock::now();
 		chrono::duration<double> duration = end - start;
 
@@ -60,3 +47,58 @@ int main() {
 		cout << dur_arr[i] << "\t";
 	}
 	cout << endl;
+	/////////////////////////////////////////////////////
+
+	cout << "duration: ";
+
+	for (int i = 0; i < m; i++)
+	{
+		for (n = 0; n < size; n++) { // -
+			Arr[n] = a--;
+		}
+		//size += 5;
+
+		auto start = chrono::high_resolution_clock::now();
+
+		merge_sort(Arr, size);
+		cout << endl;
+		auto end = chrono::high_resolution_clock::now();
+		chrono::duration<double> duration = end - start;
+
+		dur_arr[i] = duration.count();
+
+	}
+	size = 5;
+	cout << endl;
+	for (int i = 0; i < m; i++) {
+		cout << dur_arr[i] << "\t";
+	}
+	cout << endl;
+	///////////////////////////////////////////////////////////
+
+	cout << "duration: ";
+
+	for (int i = 0; i < m; i++)
+	{
+		for (n = 0; n < size; n++) { // rand
+			Arr[n] = rand() % 20;
+		}
+		//size += 5;
+
+		auto start = chrono::high_resolution_clock::now();
+
+		merge_sort(Arr, size);
+		cout << endl;
+		auto end = chrono::high_resolution_clock::now();
+		chrono::duration<double> duration = end - start;
+
+		dur_arr[i] = duration.count();
+
+	}
+	size = 5;
+	cout << endl;
+	for (int i = 0; i < m; i++) {
+		cout << dur_arr[i] << "\t";
+	}
+	cout << endl;
+}
