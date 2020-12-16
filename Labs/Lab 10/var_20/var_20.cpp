09781f2c4c98118d78447b77275c69053d7a1b97
Dmitry <dima.shevchenko10@gmail.com>
2020-12-17 00:06:29 +0200

    коректировки кода

--- Labs/Lab 10/var_20/var_20.cpp
+++ Labs/Lab 10/var_20/var_20.cpp
@@ -4,10 +4,10 @@
 #include <windows.h>
 
 using namespace std;
-int avarage (int *Arr, int rows, int cols) {
+double avarage (int *Arr, int rows, int cols) {
 	int arr[20][20]{};
 	int temp = 0; //номер елемента одномер массива
-	int AvarageValue = 0;
+	double AvarageValue = 0;
 	int max[20]{}; //массив макс чисел
 
 	cout << "Двумерный массив: " << endl;
@@ -57,27 +57,17 @@ int main() {
 	SetConsoleOutputCP(1251);
 	srand(time(NULL));
 
-
-	int size; 
-	cout << "Введите размер масива ";
-	cin >> size;
-
-	int* Arr = new int[size];
 	int N, M;
-
 	cout << "Введите количество строк и столбцов массива " ;
 	cin >> N >> M;
 	
-	while (N * M != size) {
-		cout << "Ошибка, произведение строк и столбцов должно равняться размеру массива" << endl;
-		cout << "Введите количество строк и столбцов массива ";
-		cin >> N >> M;
-		cout << endl;
-	}
+	int size = N * M;
+	int* Arr = new int[size];
+
 	cout << "Одномерный массив: " << endl;
 	for (int i = 0; i < size; i++) {
 		
-		Arr[i] = rand() & 50;
+		Arr[i] =rand() & 10;
 		cout << Arr[i] << " ";
 	}
 	cout << endl;
