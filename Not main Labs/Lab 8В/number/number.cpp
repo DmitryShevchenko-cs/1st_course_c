dd020a90bfaef39f065f019ed825c383f218283f
Dmitry <dima.shevchenko10@gmail.com>
2020-12-18 23:04:13 +0200

    8 в доделана

--- Not main Labs/Lab 8В/number/number.cpp
+++ Not main Labs/Lab 8В/number/number.cpp
@@ -1,20 +1,24 @@
 ﻿#include <iostream>
-
+#include <windows.h>
 using namespace std;
 
 int main()
 {
-    
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
     long int num_a;
     int sum=0, k=0;
     long int num_b;
     int temp;
-
+    int sumall;
+    cout << "Введите число: ";
     cin >> num_a;
+    cout << "Число наоборот: ";
     while (num_a != 0) {
         
         temp = num_a % 10;
-        cout << temp << "" ;
+        cout << temp;
        
         
         sum += temp;
@@ -24,8 +28,8 @@ int main()
 
     }
 
-    cout << "\n\n" << sum << endl;
-    cout << k << endl;
+    cout << "\n\n" <<"Сумма цифр: " << sum << endl;
+    cout <<"Кол-во цифр: "<< k << endl;
    
 
     return 0;
