844db290c1149277b55d55cbf8e661769cce2c31
Dmitry <dima.shevchenko10@gmail.com>
2021-01-04 23:48:56 +0200

    число наоборот как новая переменная

--- Not main Labs/Lab 8В/number/number.cpp
+++ Not main Labs/Lab 8В/number/number.cpp
@@ -9,27 +9,30 @@ int main()
 
     long int num_a;
     int sum=0, k=0;
-    long int num_b;
-    int temp;
-    int sumall;
+    int temp, un_num = 0;
+   
+    //354
     cout << "Введите число: ";
     cin >> num_a;
-    cout << "Число наоборот: ";
+    
     while (num_a != 0) {
         
         temp = num_a % 10;
-        cout << temp;
-       
-        
+      
+        un_num += temp;
+        un_num *= 10;
+
         sum += temp;
 
         num_a /= 10;
         k++;
 
     }
-
-    cout << "\n\n" <<"Сумма цифр: " << sum << endl;
-    cout <<"Кол-во цифр: "<< k << endl;
+    un_num /= 10;
+    cout << endl << endl;
+    cout << "Число наоборот: " << un_num << endl;
+    cout << "Сумма цифр: " << sum << endl;
+    cout << "Кол-во цифр: " << k << endl;
    
 
     return 0;
