7c58fe856cca222f077bc4d3c70ec3930d4c3971
Dmitry <dima.shevchenko10@gmail.com>
2021-02-04 23:02:42 +0200

    lab 11

---
+++ Labs/Lab 11/Var 20/Var 20.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <ctime>
+#include <windows.h>
+using namespace std;
+
+void fun(int* a, int s) {
+	cout << a[s] << '\t';
+	if (s > 0) {
+		fun(a, s - 1);
+	}
+}
+
+int main(){
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	srand(time(NULL));
+	int size;
+	
+	cin >> size;
+
+	int* A = new int[size];
+	for (int i = 0; i < size; i++) {
+		A[i] = rand() % 50;
+	}
+
+	for (int i = 0; i < size; i++) {
+		cout<<A[i]<<'\t';
+	}
+	cout << endl;
+	fun(A, size-1);
+}
+
