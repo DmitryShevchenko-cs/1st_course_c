1f1895b41c6671526cd1bedddc962618197bd447
Dmitry <dima.shevchenko10@gmail.com>
2021-02-12 13:39:40 +0200

    1/2 всей работы сделана

---
+++ Labs/2_Lab_1/var.20/var.20.cpp
@@ -0,0 +1,74 @@
+﻿#define  _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include "windows.h"
+using namespace std;
+
+struct Tabl {
+    string name;
+    char type;
+    short tem;
+    unsigned sp;
+    
+} t[4] ;
+
+int f1() {
+    
+    return 0; 
+}
+
+int f2() {
+    t[1].name = "Анилин";
+    t[2].name = "Ртуть";
+    t[3].name = "Кедровое";
+
+    t[1].type = 'Ч';
+    t[2].type = 'Ч';
+    t[3].type = 'М';
+
+    t[1].tem = 20;
+    t[2].tem = 20;
+    t[3].tem = 29;
+
+    t[1].sp = 1656;
+    t[2].sp = 1451;
+    t[3].sp = 1406;
+
+    for (int i = 1; i < 4; i++)
+        printf("| %9s | %5c | %9hd | %11u |\n",
+            t[i].name.c_str(), t[i].type, t[i].tem, t[i].sp);
+
+    printf("|---------------------------------------------|\n");
+    printf("| примечание:  ч - чистое вещество, м - масло |\n");
+    printf("-----------------------------------------------\n");
+
+    return 0;
+}
+
+
+int main(void) {
+    
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int a;
+
+    cout << "1 - ввод с экрана\n";
+    cout << "2 - случайным образом\n";
+    cin >> a;
+
+    printf("-----------------------------------------------\n");
+    printf("|          Скорость звука в жидкостях         |\n");
+    printf("|---------------------------------------------|\n");
+    printf("| Вещество  |  Тип  |Температура|  Скорость   |\n");
+    printf("|           |       |  (град.С) |   (м/сек)   |\n");
+    printf("|-----------|-------|-----------|-------------|\n");
+
+    if (a == 2) {
+        f2();
+    }
+
+    else cout << "wrong number";
+
+}
