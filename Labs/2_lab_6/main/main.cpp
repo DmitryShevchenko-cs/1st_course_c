2885b34e69c1591c4e48657dcb2b8abb1a7b04a9
Dmitry <dima.shevchenko10@gmail.com>
2021-04-07 10:49:05 +0300

    yes! i did it

--- Labs/2_lab_6/main/main.cpp
+++ Labs/2_lab_6/main/main.cpp
@@ -7,15 +7,42 @@ int main()
 
 	char STR[N+1], str[N+2];
 	char stars[]="***", zero[]="";
+
+	for (int i = 0; i < N; i++)
+	{
+		*(STR + i) = '\0';
+	}
+	printf("Введите сообщение, '***'-прекратить ввод> ");
+	scanf("%[^\n]s", STR);
+
 	while (true)
 	{
-		printf("Введите сообщение, '***'-прекратить ввод> ");
 		
-		if (!myStrcmp(myGets(STR),stars)) break;
+		if (!myStrcmp(STR,stars)) break;
 		if (!myStrcmp(STR, zero)) printf("Тут пусто (\n");
 		printf("%s\n", STR);
 		printf("%s\n", push(STR, str));
-		
+
+		int c;
+		while ((c = getchar()) != EOF && c != '\n'); /* <note the semicolon! */
+
+		if (c == EOF) {
+			if (feof(stdin)) {
+				/* Handle stdin EOF. */
+			}
+			else {
+				/* Handle stdin error. */
+			}
+		}
+
+		for (int i = 0; i < N; i++)
+		{
+			*(STR + i) = '\0';
+		}
+		printf("Введите сообщение, '***'-прекратить ввод> ");
+		scanf("%[^\n]s", STR);
+		fflush(stdin);
 	}
+	
 	return 0; 
 }
