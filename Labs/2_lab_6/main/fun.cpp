49cd3b5a96fe0a9fba1c54e624b885400412ae8c
Dmitry <dima.shevchenko10@gmail.com>
2021-04-09 11:21:59 +0300

    myStrcmp

--- Labs/2_lab_6/main/fun.cpp
+++ Labs/2_lab_6/main/fun.cpp
@@ -16,23 +16,7 @@ bool myStrcmp(char* str1, char* str2){
 		}
 	}
 
-	//int i = 0, flag = 0;
-	//while (flag == 0)
-	//{
-	//	if (*(str1 + i) == *(str2 + i))
-	//	{
-	//		flag = 1;
-	//	}
-	//	else if (*(str1 + i) != *(str2 + i))
-	//	{
-	//		flag = 0;
-	//	}
-	//	else
-	//	{
-	//		i++;
-	//	}
-	//}
-	//return flag;
+	
 }
 int myStrlen(char* str)
 {
