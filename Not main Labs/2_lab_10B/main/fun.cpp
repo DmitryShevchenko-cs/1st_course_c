de79a2c6c5d5a7242916da13d6a1fe93fb78be48
Dmitry <dima.shevchenko10@gmail.com>
2021-05-04 17:38:50 +0300

    done

---
+++ Not main Labs/2_lab_10B/main/fun.cpp
@@ -0,0 +1,16 @@
+#include "fun.h"
+
+    void SUM::get(int a, int b) {
+        x = a;
+        y = b;
+    }
+    void SUM::print() {
+        cout << "x = " << x << "  ";
+        cout << "y = " << y << "  ";
+        cout << "Sum = " << Sum << "  " << endl;
+    }
+    void SUM::sum() {
+        Sum = x + y;
+        cout << "Sum = " << Sum << endl;
+        
+    }
---
+++ Not main Labs/2_lab_10B/main/main.cpp
@@ -0,0 +1,43 @@
+﻿#include "fun.h"
+
+int main() {
+	SUM a, b(5,7), c;
+	SUM *aa, *bb, *cc;
+
+	aa = &a;
+	a.print();
+	aa->print();
+	cout << endl;
+	a.sum();
+	aa->sum();
+	cout << endl;
+	a.print();
+	aa->print();
+
+	cout << "-------------------------" << endl;
+	cout << endl;
+	bb = &b;
+	b.print();
+	bb->print();
+	cout << endl;
+	b.sum();
+	bb->sum();
+	cout << endl;
+	b.print();
+	bb->print();
+	
+	cout << "-------------------------" << endl;
+	cout << endl;
+	cc = &c;
+	c.get(4, 99);
+	c.print();
+	c.sum();
+	c.print();
+	cout << endl;
+	cc->get(3, 88);
+	cc->print();
+	cc->sum();
+	cc->print();
+
+	return 0;
+}
