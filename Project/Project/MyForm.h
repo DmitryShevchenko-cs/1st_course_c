f06c328586e517f068130db5781ffa785ab47381
Dmitry <dima.shevchenko10@gmail.com>
2021-05-19 00:52:11 +0300

    123

--- Project/Project/MyForm.h
+++ Project/Project/MyForm.h
@@ -172,9 +172,11 @@ namespace Project {
 			// 
 			// textBox3
 			// 
+			this->textBox3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
 			this->textBox3->Location = System::Drawing::Point(31, 223);
 			this->textBox3->Name = L"textBox3";
-			this->textBox3->Size = System::Drawing::Size(171, 22);
+			this->textBox3->Size = System::Drawing::Size(171, 27);
 			this->textBox3->TabIndex = 31;
 			// 
 			// button3
@@ -199,18 +201,22 @@ namespace Project {
 			// 
 			// textBoxD
 			// 
-			this->textBoxD->Location = System::Drawing::Point(302, 156);
+			this->textBoxD->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBoxD->Location = System::Drawing::Point(297, 155);
 			this->textBoxD->Name = L"textBoxD";
-			this->textBoxD->Size = System::Drawing::Size(42, 22);
+			this->textBoxD->Size = System::Drawing::Size(42, 27);
 			this->textBoxD->TabIndex = 28;
 			this->textBoxD->Text = L"0";
 			// 
 			// label6
 			// 
 			this->label6->AutoSize = true;
-			this->label6->Location = System::Drawing::Point(278, 159);
+			this->label6->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label6->Location = System::Drawing::Point(273, 158);
 			this->label6->Name = L"label6";
-			this->label6->Size = System::Drawing::Size(16, 17);
+			this->label6->Size = System::Drawing::Size(18, 20);
 			this->label6->TabIndex = 27;
 			this->label6->Text = L"d";
 			// 
@@ -260,25 +266,31 @@ namespace Project {
 			// 
 			// textBoxB
 			// 
-			this->textBoxB->Location = System::Drawing::Point(230, 155);
+			this->textBoxB->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBoxB->Location = System::Drawing::Point(215, 155);
 			this->textBoxB->Name = L"textBoxB";
-			this->textBoxB->Size = System::Drawing::Size(42, 22);
+			this->textBoxB->Size = System::Drawing::Size(42, 27);
 			this->textBoxB->TabIndex = 22;
 			this->textBoxB->Text = L"0";
 			// 
 			// textBoxA
 			// 
-			this->textBoxA->Location = System::Drawing::Point(230, 124);
+			this->textBoxA->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBoxA->Location = System::Drawing::Point(215, 124);
 			this->textBoxA->Name = L"textBoxA";
-			this->textBoxA->Size = System::Drawing::Size(42, 22);
+			this->textBoxA->Size = System::Drawing::Size(42, 27);
 			this->textBoxA->TabIndex = 21;
 			this->textBoxA->Text = L"1";
 			// 
 			// textBoxC
 			// 
-			this->textBoxC->Location = System::Drawing::Point(302, 124);
+			this->textBoxC->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBoxC->Location = System::Drawing::Point(297, 123);
 			this->textBoxC->Name = L"textBoxC";
-			this->textBoxC->Size = System::Drawing::Size(42, 22);
+			this->textBoxC->Size = System::Drawing::Size(42, 27);
 			this->textBoxC->TabIndex = 20;
 			this->textBoxC->Text = L"0";
 			// 
@@ -328,17 +340,21 @@ namespace Project {
 			// 
 			// textBox2
 			// 
-			this->textBox2->Location = System::Drawing::Point(64, 152);
+			this->textBox2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox2->Location = System::Drawing::Point(64, 155);
 			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(42, 22);
+			this->textBox2->Size = System::Drawing::Size(42, 27);
 			this->textBox2->TabIndex = 9;
 			this->textBox2->Text = L"10";
 			// 
 			// textBox1
 			// 
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
 			this->textBox1->Location = System::Drawing::Point(64, 124);
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(42, 22);
+			this->textBox1->Size = System::Drawing::Size(42, 27);
 			this->textBox1->TabIndex = 8;
 			this->textBox1->Text = L"-10";
 			// 
@@ -356,55 +372,67 @@ namespace Project {
 			// label5
 			// 
 			this->label5->AutoSize = true;
-			this->label5->Location = System::Drawing::Point(278, 127);
+			this->label5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label5->Location = System::Drawing::Point(273, 126);
 			this->label5->Name = L"label5";
-			this->label5->Size = System::Drawing::Size(15, 17);
+			this->label5->Size = System::Drawing::Size(18, 20);
 			this->label5->TabIndex = 5;
 			this->label5->Text = L"c";
 			// 
 			// label4
 			// 
 			this->label4->AutoSize = true;
-			this->label4->Location = System::Drawing::Point(206, 158);
+			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label4->Location = System::Drawing::Point(191, 158);
 			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(16, 17);
+			this->label4->Size = System::Drawing::Size(18, 20);
 			this->label4->TabIndex = 4;
 			this->label4->Text = L"b";
 			// 
 			// label3
 			// 
 			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(206, 127);
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label3->Location = System::Drawing::Point(191, 127);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(16, 17);
+			this->label3->Size = System::Drawing::Size(18, 20);
 			this->label3->TabIndex = 3;
 			this->label3->Text = L"a";
 			// 
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(28, 154);
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label2->Location = System::Drawing::Point(28, 157);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(31, 17);
+			this->label2->Size = System::Drawing::Size(37, 20);
 			this->label2->TabIndex = 2;
 			this->label2->Text = L"До:";
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
 			this->label1->Location = System::Drawing::Point(28, 127);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(30, 17);
+			this->label1->Size = System::Drawing::Size(37, 20);
 			this->label1->TabIndex = 1;
 			this->label1->Text = L"От:";
 			// 
 			// comboBox1
 			// 
+			this->comboBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
 			this->comboBox1->FormattingEnabled = true;
 			this->comboBox1->Items->AddRange(gcnew cli::array< System::Object^  >(4) { L"y=ax+b", L"y=ax²+bx+c", L"y=asin(bx+c)+d", L"y=(√ax + b)+c" });
 			this->comboBox1->Location = System::Drawing::Point(31, 54);
 			this->comboBox1->Name = L"comboBox1";
-			this->comboBox1->Size = System::Drawing::Size(121, 24);
+			this->comboBox1->Size = System::Drawing::Size(139, 28);
 			this->comboBox1->TabIndex = 0;
 			this->comboBox1->Text = L"Не выбрано";
 			// 
@@ -1144,6 +1172,7 @@ private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e
 		break;
 	}
 }
+
 
 };
 }
